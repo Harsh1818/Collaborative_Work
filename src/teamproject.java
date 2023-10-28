fe100189003c89afcb048e19341874f3fafdc522
JaswanthPerni <jaswanth7567@yahoo.com>
2023-10-28 22:51:32 +0530

    This function is to greet the users

--- src/teamproject.java
+++ src/teamproject.java
@@ -1,5 +1,16 @@
 public class SampleClass {
     public static void main(String[] args) {
         System.out.println("Hello from the Team Lead!");
+        int number = 5;
+        System.out.println("Square of " + number + " is: " + square(number));
+        greet("Dev2");
+    }
+
+    public static int square(int x) {
+        return x * x;
+    }
+
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 }
