c865d489423b785e0bb41ee831aa7f205b86d90a
Kumar Harshdeep <89003889+Harsh1818@users.noreply.github.com>
2023-10-28 23:20:58 +0530

    merged to teamproject.java

--- src/teamproject.java
+++ src/teamproject.java
@@ -6,15 +6,12 @@ public class SampleClass {
         greet("Dev3");
         printNumbers();
     }
-
     public static int square(int x) {
         return x * x;
     }
-
     public static void greet(String name) {
         System.out.println("Hello, " + name + "!");
     }
-
     public static void printNumbers() {
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
