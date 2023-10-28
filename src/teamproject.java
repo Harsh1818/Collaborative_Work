82ec0f3bee5c958d2c9f7390b47b6f3e31a0897f
unknown <udaylabishetty2102@gmail.com>
2023-10-28 22:23:12 +0530

    This function is to find the square of the number

--- src/teamproject.java
+++ src/teamproject.java
@@ -3,3 +3,15 @@ public class SampleClass {
         System.out.println("Hello from the Team Lead!");
     }
 }
+
+public class SampleClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from the Team Lead!");
+        int number = 5;
+        System.out.println("Square of " + number + " is: " + square(number));
+    }
+
+    public static int square(int x) {
+        return x * x;
+    }
+}
